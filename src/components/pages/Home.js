4eeeaaab423df780d5d8eb1b5d26e76977f74e43
puneetshivaay <puneetshivaay@gmail.com>
2022-05-31 00:27:29 +0530

    FrontEnd Files

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
                 <div className="row">
                     <div className="col-xs-12 col-md-6">
                         <div className="bg bg-left">
-                            <a className="nav-link" href="/userinfo"><h2>Speak</h2></a>
+                            <a className="nav-link" href="/userinfo"><h2>Speak </h2></a>
                         </div>
                     </div>
                     <div className="col-xs-12 col-md-6">
